8e2259d0d0b8851d4c9072e371db603306119ed5
TÖRÖK Attila <torokati44@gmail.com>
2021-01-20 21:19:30 +0100

    video/h263: Special case the edge pixels in the YUV -> RGBA conversion step
    This yielded an overall 20% faster decoding speed on the video I tested

--- yuv/src/bt601.rs
+++ yuv/src/bt601.rs
@@ -8,42 +8,64 @@ pub fn clamped_index(width: i32, height: i32, x: i32, y: i32) -> usize {
     (x.max(0).min(width - 1) + (y.max(0).min(height - 1) * width)) as usize
 }
 
+pub fn unclamped_index(width: i32, x: i32, y: i32) -> usize {
+    (x + y * width) as usize
+}
+
 pub fn sample_chroma_for_luma(
     chroma: &[u8],
     chroma_width: usize,
+    chroma_height: usize,
     luma_x: usize,
     luma_y: usize,
+    clamp: bool,
 ) -> u8 {
     let width = chroma_width as i32;
-    let height = chroma.len() as i32 / width;
+    let height = chroma_height as i32;
 
-    let chroma_x = if luma_x == 0 {
-        -1
-    } else {
-        (luma_x as i32 - 1) / 2
-    };
-    let chroma_y = if luma_y == 0 {
-        -1
-    } else {
-        (luma_y as i32 - 1) / 2
-    };
+    let sample_00;
+    let sample_01;
+    let sample_10;
+    let sample_11;
 
-    let sample_00 = chroma
-        .get(clamped_index(width, height, chroma_x, chroma_y))
-        .copied()
-        .unwrap_or(0) as u16;
-    let sample_10 = chroma
-        .get(clamped_index(width, height, chroma_x + 1, chroma_y))
-        .copied()
-        .unwrap_or(0) as u16;
-    let sample_01 = chroma
-        .get(clamped_index(width, height, chroma_x, chroma_y + 1))
-        .copied()
-        .unwrap_or(0) as u16;
-    let sample_11 = chroma
-        .get(clamped_index(width, height, chroma_x + 1, chroma_y + 1))
-        .copied()
-        .unwrap_or(0) as u16;
+    if clamp {
+        let chroma_x = if luma_x == 0 {
+            -1
+        } else {
+            (luma_x as i32 - 1) / 2
+        };
+        let chroma_y = if luma_y == 0 {
+            -1
+        } else {
+            (luma_y as i32 - 1) / 2
+        };
+
+        sample_00 = chroma
+            .get(clamped_index(width, height, chroma_x, chroma_y))
+            .copied()
+            .unwrap_or(0) as u16;
+        sample_10 = chroma
+            .get(clamped_index(width, height, chroma_x + 1, chroma_y))
+            .copied()
+            .unwrap_or(0) as u16;
+        sample_01 = chroma
+            .get(clamped_index(width, height, chroma_x, chroma_y + 1))
+            .copied()
+            .unwrap_or(0) as u16;
+        sample_11 = chroma
+            .get(clamped_index(width, height, chroma_x + 1, chroma_y + 1))
+            .copied()
+            .unwrap_or(0) as u16;
+    } else {
+        let chroma_x = (luma_x as i32 - 1) / 2;
+        let chroma_y = (luma_y as i32 - 1) / 2;
+
+        let base = unclamped_index(width, chroma_x, chroma_y);
+        sample_00 = chroma.get(base).copied().unwrap_or(0) as u16;
+        sample_10 = chroma.get(base + 1).copied().unwrap_or(0) as u16;
+        sample_01 = chroma.get(base + chroma_width).copied().unwrap_or(0) as u16;
+        sample_11 = chroma.get(base + chroma_width + 1).copied().unwrap_or(0) as u16;
+    }
 
     let interp_left = luma_x % 2 != 0;
     let interp_top = luma_y % 2 != 0;
@@ -107,15 +129,19 @@ pub fn yuv420_to_rgba(
     br_width: usize,
 ) -> Vec<u8> {
     let y_height = y.len() / y_width;
+    let br_height = chroma_b.len() / br_width;
 
     let mut rgba = Vec::new();
     rgba.resize(y.len() * 4, 0);
 
-    for y_pos in 0..y_height {
-        for x_pos in 0..y_width {
+    // do the bulk of the pixels faster, with no clamping, leaving out the edges
+    for y_pos in 1..y_height - 1 {
+        for x_pos in 1..y_width - 1 {
             let y_sample = y.get(x_pos + y_pos * y_width).copied().unwrap_or(0) as f32;
-            let b_sample = sample_chroma_for_luma(chroma_b, br_width, x_pos, y_pos) as f32;
-            let r_sample = sample_chroma_for_luma(chroma_r, br_width, x_pos, y_pos) as f32;
+            let b_sample =
+                sample_chroma_for_luma(chroma_b, br_width, br_height, x_pos, y_pos, false) as f32;
+            let r_sample =
+                sample_chroma_for_luma(chroma_r, br_width, br_height, x_pos, y_pos, false) as f32;
 
             convert_and_write_pixel(
                 (y_sample, b_sample, r_sample),
@@ -123,6 +149,44 @@ pub fn yuv420_to_rgba(
                 y_width,
                 x_pos,
                 y_pos,
+            );
+        }
+    }
+
+    // doing the sides with clamping
+    for y_pos in 0..y_height {
+        for x_pos in [0, y_width - 1].iter() {
+            let y_sample = y.get(x_pos + y_pos * y_width).copied().unwrap_or(0) as f32;
+            let b_sample =
+                sample_chroma_for_luma(chroma_b, br_width, br_height, *x_pos, y_pos, true) as f32;
+            let r_sample =
+                sample_chroma_for_luma(chroma_r, br_width, br_height, *x_pos, y_pos, true) as f32;
+
+            convert_and_write_pixel(
+                (y_sample, b_sample, r_sample),
+                &mut rgba,
+                y_width,
+                *x_pos,
+                y_pos,
+            );
+        }
+    }
+
+    // doing the top and bottom edges with clamping
+    for x_pos in 0..y_width {
+        for y_pos in [0, y_height - 1].iter() {
+            let y_sample = y.get(x_pos + y_pos * y_width).copied().unwrap_or(0) as f32;
+            let b_sample =
+                sample_chroma_for_luma(chroma_b, br_width, br_height, x_pos, *y_pos, true) as f32;
+            let r_sample =
+                sample_chroma_for_luma(chroma_r, br_width, br_height, x_pos, *y_pos, true) as f32;
+
+            convert_and_write_pixel(
+                (y_sample, b_sample, r_sample),
+                &mut rgba,
+                y_width,
+                x_pos,
+                *y_pos,
             );
         }
     }
